39ea3c762acb4ff418c6746971e6148f00d79a07
Taufiq <taufiqmmhd@gmail.com>
2021-01-16 23:30:27 +0800

    fix: reposition close icon & fit images in preview
    Previews of uploads had their close icon button at the corner of the
    screen due to css issues. The images also overflowed out of the container.
    
    This commit fixes those issues.

--- src/PhotoUploadListItem.jsx
+++ src/PhotoUploadListItem.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function PhotoUploadListItem({ photo, onClose }) {
   return (
-    <div className="flex-column mr-2" style={{width: '47%', height: '100%', flexWrap: 'wrap'}}>
-        <div className="border flex-column rounded p-1 bg-white">
+    <div className="flex-column mr-2" style={{width: '40%', height: '100%', flexWrap: 'wrap'}}>
+        <div className="position-relative border flex-column rounded p-1 bg-white">
           <i
             className="fas fa-times-circle position-absolute"
             style={{ right: -16, top: -16, fontSize: '32px'}}
             onClick={onClose}
           />
-          <img alt={""} src={photo} style={{ height: '100%', width: '100%' }}/>
+          <img alt={""} src={photo} style={{ height: '100px', width: '100%', objectFit: 'contain'}} />
       </div>
     </div>
   ) 
