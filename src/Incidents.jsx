881e2070861bb7652d8f10d341bbd95d5caa32f3
Taufiq <taufiqmmhd@gmail.com>
2021-03-05 17:02:19 +0800

    feat: include station, rota & duty date in generated pptx filename

--- src/Incidents.jsx
+++ src/Incidents.jsx
@@ -1,6 +1,11 @@
-import { Card, Container, Button, Navbar } from 'react-bootstrap';
+import { Card, Container, Button, Navbar, Form, Col } from 'react-bootstrap';
 import _ from 'lodash';
 import * as PptxGenerator from './pptx/Generator';
+import Datetime from 'react-datetime';
+import "react-datetime/css/react-datetime.css";
+import { Controller, useForm } from 'react-hook-form'
+import moment from 'moment'
+
 
 function IncidentCard({ incident, onSelectIncident, onDeleteIncident }) {
   const photos = _.chain(incident)
@@ -45,7 +50,13 @@ function IncidentCard({ incident, onSelectIncident, onDeleteIncident }) {
 function Incidents({
   incidents, onCreateIncident, onSelectIncident, onDeleteIncident,
 }) {
+  const { register, handleSubmit, getValues, setValue, control } = useForm({
+    defaultValues: {
+      dutyDate: moment()
+    }
+  })
   async function generateSlides() {
+    const { station, rota, dutyDate } = getValues()
     const generatedPptx = PptxGenerator.createPowerPoint();
     for (const incident of incidents) {
       await PptxGenerator.populateWithImages(generatedPptx, {
@@ -55,7 +66,7 @@ function Incidents({
         others: incident.others,
       }, incident.incident_no);
     }
-    PptxGenerator.savePowerPoint(generatedPptx, 'DECAM.pptx');
+    PptxGenerator.savePowerPoint(generatedPptx, `S${station}_R${rota}_${dutyDate.format('DDMMYYYY')}`);
   }
   return (
     <>
@@ -64,6 +75,55 @@ function Incidents({
           <Navbar.Brand color="light" className="mx-auto">DECAM Slidedeck Generator</Navbar.Brand>
         </Navbar>
       </div>
+      <Container className="mt-3">
+        <Card>
+          <Card.Body>
+            <Form>
+              <Form.Row>
+                <Col>
+                  <Form.Group>
+                    <Form.Label>Station</Form.Label>
+                    <Form.Control as="select" ref={register} name="station">
+                      <option>41</option>
+                      <option>42</option>
+                      <option>43</option>
+                      <option>44</option>
+                      <option>45</option>
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group>
+                    <Form.Label>Rota</Form.Label>
+                    <Form.Control as="select" ref={register} name="rota">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group>
+                  <Form.Label>Duty Date</Form.Label>
+                  <Controller
+                    control={control}
+                    name="dutyDate"
+                    render={({ onChange, value }) => (
+                      <Datetime
+                        value={value}
+                        timeFormat=""
+                        onChange={(newDate) => onChange(newDate)}
+                      />
+                    )
+                    }
+                  />
+                </Form.Group>
+              </Form.Row>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
       <Container>
         {
           incidents.map((incident) => (
