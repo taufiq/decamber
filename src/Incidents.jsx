16710f27c612d175a735488d836fd856968aa5d8
Taufiq <taufiqmmhd@gmail.com>
2021-03-28 06:43:05 +0800

    refactor: remove unused useDebounce hook

--- src/Incidents.jsx
+++ src/Incidents.jsx
@@ -51,22 +51,6 @@ function IncidentCard({ incident, onSelectIncident, onDeleteIncident }) {
   );
 }
 
-function useDebounce(value) {
-  const [debouncedValue, setDebouncedValue] = useState(value)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setDebouncedValue(value)
-    }, 1500)
-
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [value])
-
-  return debouncedValue
-}
-
 function shallowCompare(objectA, objectB) {
   if (!_.isEqual(_.keys(objectA), _.keys(objectB))) {
     return false
