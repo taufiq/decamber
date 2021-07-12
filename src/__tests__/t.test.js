4d4e6a00983b3468616a912d407455a5929c386f
Taufiq <taufiqmmhd@gmail.com>
2021-07-13 01:26:45 +0800

    test: check for form inputs presence and card shake on submission

--- src/__tests__/t.test.js
+++ src/__tests__/t.test.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { act, getByTestId, render, waitFor } from '@testing-library/react';
+import { act, getByTestId, render, waitFor, screen, fireEvent, cleanup } from '@testing-library/react';
 import Incidents from '../Incidents';
+import userEvent from '@testing-library/user-event'
+import App from '../App'
+import * as IDBManager from 'idb-keyval';
 
-test('Shows general information form', async () => {
-    render(<Incidents incidents={[]}/>)
+test('Renders Incident', async () => {
+    render(<App />)
 
-});
+    const station = await screen.findByLabelText('Station');
+    const rota = await screen.findByLabelText('Rota');
+    const callSign = await screen.findByLabelText('Call Sign');
+    const sectionCommander = await screen.findByLabelText('SC');
+    const pumpOperator = await screen.findByLabelText('PO');
+
+    const dutyDate = await screen.findByLabelText('Duty Date');
+    expect(station).toBeInTheDocument();
+    expect(rota).toBeInTheDocument();
+    expect(callSign).toBeInTheDocument();
+    expect(sectionCommander).toBeInTheDocument();
+    expect(pumpOperator).toBeInTheDocument();
+    expect(dutyDate).toBeInTheDocument();
+})
+
+test('Shakes on no incidents', async () => {
+    render(<App />)
+    const createIncidentCard = screen.getByText('Add Incident').closest(".card")
+    userEvent.click(screen.getByText('Generate Powerpoint'))
+    expect(createIncidentCard).toHaveClass('shake')
+    await waitFor(() => { expect(createIncidentCard).not.toHaveClass('shake')})
+})
