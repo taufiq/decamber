17b1215734d0567e82f6e921113ca5a6a0756d8d
Taufiq <taufiqmmhd@gmail.com>
2021-07-14 01:06:31 +0800

    test: add redirect to create incident form test

--- src/__tests__/t.test.js
+++ src/__tests__/t.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event'
 import App from '../App'
 import * as IDBManager from 'idb-keyval';
 
-test('Renders Incident', async () => {
+test('Renders Incidents screen', async () => {
     render(<App />)
 
     const station = await screen.findByLabelText('Station');
@@ -13,14 +13,37 @@ test('Renders Incident', async () => {
     const callSign = await screen.findByLabelText('Call Sign');
     const sectionCommander = await screen.findByLabelText('SC');
     const pumpOperator = await screen.findByLabelText('PO');
-
     const dutyDate = await screen.findByLabelText('Duty Date');
+
+    const generatePowerpointButton = await screen.findByRole('button', {name: /generate powerpoint/i})
+    const resetButton = await screen.findByRole('button', {name: /reset/i})
+
+    const createIncidentCard = (await screen.findByText('Add Incident')).closest('.card')
+
     expect(station).toBeInTheDocument();
     expect(rota).toBeInTheDocument();
     expect(callSign).toBeInTheDocument();
     expect(sectionCommander).toBeInTheDocument();
     expect(pumpOperator).toBeInTheDocument();
     expect(dutyDate).toBeInTheDocument();
+
+    expect(generatePowerpointButton).toBeInTheDocument();
+    expect(resetButton).toBeInTheDocument();
+    expect(createIncidentCard).toBeInTheDocument();
+})
+
+test('Redirects to create incident form', async () => {
+    render(<App />);
+    const createIncidentCard = screen.getByText('Add Incident').closest(".card");
+    userEvent.click(createIncidentCard);
+
+    const cancelButton = await screen.findByRole('button', {name: /cancel/i})
+    const submitButton = await screen.findByRole('button', {name: /submit/i})
+    const header = await screen.findByText('Add Incident')
+
+    expect(cancelButton).toBeInTheDocument()
+    expect(submitButton).toBeInTheDocument()
+    expect(header).toBeInTheDocument()
 })
 
 test('Shakes on no incidents', async () => {
