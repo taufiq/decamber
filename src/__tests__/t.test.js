de2368ba3de85c6ae450e81ee98e511ea9888e60
Taufiq <taufiqmmhd@gmail.com>
2022-04-15 23:47:54 +0800

    docs: add context for unfinished test

--- src/__tests__/t.test.js
+++ src/__tests__/t.test.js
@@ -19,8 +19,8 @@ test('Renders Incidents screen', async () => {
     const pumpOperator = await screen.findByLabelText('PO');
     const dutyDate = await screen.findByLabelText('Duty Date');
 
-    const generatePowerpointButton = await screen.findByRole('button', {name: /generate powerpoint/i})
-    const resetButton = await screen.findByRole('button', {name: /reset/i})
+    const generatePowerpointButton = await screen.findByRole('button', { name: /generate powerpoint/i })
+    const resetButton = await screen.findByRole('button', { name: /reset/i })
 
     const createIncidentCard = (await screen.findByText('Add Incident')).closest('.card')
 
@@ -41,8 +41,8 @@ test('Redirects to create incident form', async () => {
     const createIncidentCard = screen.getByText('Add Incident').closest(".card");
     userEvent.click(createIncidentCard);
 
-    const cancelButton = await screen.findByRole('button', {name: /cancel/i})
-    const submitButton = await screen.findByRole('button', {name: /submit/i})
+    const cancelButton = await screen.findByRole('button', { name: /cancel/i })
+    const submitButton = await screen.findByRole('button', { name: /submit/i })
     const header = await screen.findByText('Add Incident')
 
     expect(cancelButton).toBeInTheDocument()
@@ -55,7 +55,7 @@ test('Goes back to Incidents page on cancellling incident creation', async () =>
     const createIncidentCard = screen.getByText('Add Incident').closest(".card");
     userEvent.click(createIncidentCard);
 
-    const cancelButton = await screen.findByRole('button', {name: /cancel/i})
+    const cancelButton = await screen.findByRole('button', { name: /cancel/i })
 
     userEvent.click(cancelButton)
 
@@ -70,14 +70,14 @@ test('Saves incident on creation', async () => {
     const incidentNoInput = await screen.findByLabelText('Incident No.');
     userEvent.type(incidentNoInput, 'Test Incident');
 
-    const submitButton = await screen.findByRole('button', {name: /submit/i})
+    const submitButton = await screen.findByRole('button', { name: /submit/i })
     userEvent.click(submitButton)
 
     await waitFor(async () => expect((await screen.findByText('Add Incident')).closest(".card")).toBeInTheDocument())
     expect(await screen.findByText('Incident No.')).toBeInTheDocument()
     expect(await screen.findByText('Test Incident')).toBeInTheDocument()
-    expect(await screen.findByRole('button', {name: /edit/i})).toBeInTheDocument()
-    expect(await screen.findByRole('button', {name: /delete/i})).toBeInTheDocument()
+    expect(await screen.findByRole('button', { name: /edit/i })).toBeInTheDocument()
+    expect(await screen.findByRole('button', { name: /delete/i })).toBeInTheDocument()
 
     // Check if values saved on default are the following
     const values = await IDBManager.values()
@@ -110,12 +110,12 @@ test('Deletes saved incident', async () => {
     const incidentNoInput = await screen.findByLabelText('Incident No.');
     userEvent.type(incidentNoInput, 'Test Incident');
 
-    const submitButton = await screen.findByRole('button', {name: /submit/i})
+    const submitButton = await screen.findByRole('button', { name: /submit/i })
     userEvent.click(submitButton)
 
     await waitFor(async () => expect((await screen.findByText('Add Incident')).closest(".card")).toBeInTheDocument())
-    const editButton = await screen.findByRole('button', {name: /edit/i})
-    const deleteButton = await screen.findByRole('button', {name: /delete/i})
+    const editButton = await screen.findByRole('button', { name: /edit/i })
+    const deleteButton = await screen.findByRole('button', { name: /delete/i })
 
     userEvent.click(deleteButton)
 
@@ -142,10 +142,11 @@ test('Saves on general information edit', async () => {
         expect((await IDBManager.get('GENERAL_INFORMATION'))).toBeTruthy()
     }, { timeout: 600 })
 
+    // Enter add incident page and exit out to check if general information edit was auto-saved
     const createIncidentCard = screen.getByText('Add Incident').closest(".card");
     userEvent.click(createIncidentCard)
 
-    const cancelButton = await screen.findByRole('button', {name: /cancel/i})
+    const cancelButton = await screen.findByRole('button', { name: /cancel/i })
     userEvent.click(cancelButton)
 
     expect(pumpOperator.value).toBe('SGT Shami')
@@ -163,32 +164,37 @@ test('Saves on general information edit', async () => {
 
 })
 
+// TODO: to add a test that a general information edit saves on navigation
+// had to do with a bug where if a user types and navigates away fast enough before auto-save kicked in
+// general information wouldn't be saved
 // test('Saves on general information edit and immediate redirect to create incident', async () => {
-//     render(<App />)
+// render(<App />)
 
-//     const station = await screen.findByDisplayValue('11');
-//     const rota = await screen.findByDisplayValue('1');
-//     const callSign = await screen.findByLabelText('Call Sign');
-//     const sectionCommander = await screen.findByLabelText('SC');
-//     const pumpOperator = await screen.findByLabelText('PO');
-//     const dutyDate = await screen.findByLabelText('Duty Date');
+// const station = await screen.findByDisplayValue('11');
+// const rota = await screen.findByDisplayValue('1');
+// const callSign = await screen.findByLabelText('Call Sign');
+// const sectionCommander = await screen.findByLabelText('SC');
+// const pumpOperator = await screen.findByLabelText('PO');
+// const dutyDate = await screen.findByLabelText('Duty Date');
 
-//     userEvent.type(pumpOperator, 'SGT Shami')
-//     userEvent.type(sectionCommander, 'SGT Fai')
-//     userEvent.type(callSign, 'PL421')
+// userEvent.type(pumpOperator, 'SGT Shami')
+// userEvent.type(sectionCommander, 'SGT Fai')
+// userEvent.type(callSign, 'PL421')
 
-//     const createIncidentCard = screen.getByText('Add Incident').closest(".card");
-//     userEvent.click(createIncidentCard)
+// const createIncidentCard = screen.getByText('Add Incident').closest(".card");
+// userEvent.click(createIncidentCard)
 
-//     const cancelButton = await screen.findByRole('button', {name: /cancel/i})
-//     userEvent.click(cancelButton)
+// const cancelButton = await screen.findByRole('button', { name: /cancel/i })
+// userEvent.click(cancelButton)
 
-//     const storedGeneralInformation = await IDBManager.get('GENERAL_INFORMATION')
-//     expect(storedGeneralInformation.pumpOperator).toBe('SGT Shami')
-//     expect(storedGeneralInformation.sectionCommander).toBe('SGT Fai')
-//     expect(storedGeneralInformation.callSign).toBe('PL421')
-//     expect(storedGeneralInformation.station).toBe('11')
-//     expect(storedGeneralInformation.rota).toBe('1')
+// screen.debug()
+
+// const storedGeneralInformation = await IDBManager.get('GENERAL_INFORMATION')
+// expect(storedGeneralInformation.pumpOperator).toBe('SGT Shami')
+// expect(storedGeneralInformation.sectionCommander).toBe('SGT Fai')
+// expect(storedGeneralInformation.callSign).toBe('PL421')
+// expect(storedGeneralInformation.station).toBe('11')
+// expect(storedGeneralInformation.rota).toBe('1')
 
 // })
 
@@ -198,5 +204,5 @@ test('Shakes on no incidents', async () => {
     const createIncidentCard = screen.getByText('Add Incident').closest(".card")
     userEvent.click(screen.getByText('Generate Powerpoint'))
     expect(createIncidentCard).toHaveClass('shake')
-    await waitFor(() => { expect(createIncidentCard).not.toHaveClass('shake')})
+    await waitFor(() => { expect(createIncidentCard).not.toHaveClass('shake') })
 })
